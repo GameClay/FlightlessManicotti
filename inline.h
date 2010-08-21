75d14e7b1a0714a9a17317257e61ecd3735a2ea7
Pat Wilson <zerostride@gmail.com>
2010-08-21 09:41:44 -0700

    Added Apache2 license.

--- inline.h
+++ inline.h
@@ -1,11 +1,28 @@
-#ifndef _INLINE_H
-#define _INLINE_H
+/* FlightlessManicotti -- Copyright (C) 2009-2010 GameClay LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#ifndef GC_INLINE_H
+#define GC_INLINE_H
 
 // http://en.wikipedia.org/wiki/C99
 #if __STDC_VERSION__ >= 199901L
    // "inline" is a keyword
 #else
-#  define inline static
+#  ifndef inline
+#     define inline static
+#  endif
 #endif
 
 #endif
