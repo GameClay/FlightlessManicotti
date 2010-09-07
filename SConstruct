01db59b6075b621ebe085aa528eaded056c32974
Pat Wilson <zerostride@gmail.com>
2010-09-06 21:58:18 -0700

    More cleaning-up.

--- SConstruct
+++ SConstruct
@@ -1,6 +1,15 @@
 from SCons.Util import flatten
 import os
+import sys
 
+# Bring in additional standard C requirements
+additional_std_lib_includes = ['std/C1x']
+
+if (sys.platform == 'win32' or sys.platform == 'cygwin'): # TODO: Change this to Visual Studio, and check by-version
+    additional_std_lib_includes += ['std/C99']
+Export('additional_std_lib_includes')   
 env = Environment(CXXFILESUFFIX='.cpp')
+
 Export('env')
+
 SConscript('SConscript', build_dir='.build')
--- site_scons/site_init.py
+++ site_scons/site_init.py
@@ -33,30 +33,3 @@ def FindAllSourceFiles(self, target):
 
     return sources, headers    
 SConsEnvironment.FindAllSourceFiles = FindAllSourceFiles
-
-# Auto-build a header which declares all of the module initialization
-# functions from SWIG, and provides a macro to call them all.
-def BuildSwigHeader(target, source, env):
-   t_name = str(target[0])
-   
-   # Auto-generate header for swig files
-   swig_hdr = open(t_name, "w")
-
-   swig_hdr.write("#ifndef _SWIG_AUTOGEN_H_\n")
-   swig_hdr.write("#define _SWIG_AUTOGEN_H_\n")
-
-   for f in source:
-      (dirName, fileName) = os.path.split(str(f))
-      (fileBaseName, fileExtension)=os.path.splitext(fileName)
-      swig_hdr.write("extern int luaopen_" + fileBaseName + "(lua_State* L);\n")
-
-   swig_hdr.write("\n#define LOAD_SWIG_LIBS(L) {\\\n");
-   for f in source:
-      (dirName, fileName) = os.path.split(str(f))
-      (fileBaseName, fileExtension)=os.path.splitext(fileName)
-      swig_hdr.write("luaopen_" + fileBaseName +"(L);\\\n") 
-
-   swig_hdr.write("}\n\n")
-   swig_hdr.write("#endif // _SWIG_AUTOGEN_\n")
-   swig_hdr.close()
-SConsEnvironment.BuildSwigHeader = BuildSwigHeader
