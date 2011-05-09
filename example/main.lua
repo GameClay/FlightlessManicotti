f9c6a4b5f521f9053462e2927217c7b60c1d37c2
Pat Wilson <zerostride@gmail.com>
2011-05-09 13:25:52 -0700

    Split out event handler into it's own sub module

--- example/main.lua
+++ example/main.lua
@@ -11,6 +11,7 @@ end
 -- Bring in script library
 require 'script'
 require 'game.components'
+require 'events'
 
 -- Main is executed only once, it is not a loop. It receives the arguments
 -- that were passed to the 'kl_initialize' function.
@@ -39,35 +40,3 @@ function main(...)
    local test = Component.new()
    print("I made a component! Is it registered? "..tostring(test:isregistered()))
 end
-
--- Test registration of an event
-eof_evt_id = Events.register(SCTX, "EOF")
-advance_time_id = Events.register(SCTX, "AdvanceTime")
-process_tick_id = Events.register(SCTX, "ProcessTick")
-
--- Define and assign event-handler.
-function event_handler()
-   -- dequeue everything from code
-   id,context,arg = Events.dequeue(SCTX)
-   while (not id or not (id == eof_evt_id)) do
-      if id then
-         if id == advance_time_id then
-            arg = Cast.int_to_float(arg)
-            
-            -- Pass to script advance time list
-         elseif id == process_tick_id then
-            -- Pass to script process tick list
-         else
-            print("From code: {"..id..","..tostring(context)..","..arg.."}")
-         end
-      end
-      
-      id,context,arg = Events.dequeue(SCTX)
-   end
-
-   -- If we just hit EOF, acknowledge the EOF
-   if(id == eof_evt_id) then
-      Events.framedone(SCTX, context)
-   end
-end
-Events.handler = event_handler
---
+++ script/events/Handler.lua
@@ -0,0 +1,42 @@
+-- -*- Mode: lua; tab-width: 3; c-basic-offset: 3; indent-tabs-mode: nil -*-
+-- vim: set filetype=lua tabstop=3 softtabstop=3 shiftwidth=3 expandtab:
+
+-- FlightlessManicotti -- Copyright (C) 2010-2011 GameClay LLC
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- Define and assign event-handler.
+Events.handler = function()
+   -- dequeue everything from code
+   id,context,arg = Events.dequeue(SCTX)
+   while (not id or not (id == Events.eof)) do
+      if id then
+         if id == Events.advancetime then
+            arg = Cast.int_to_float(arg)
+            
+            -- Pass to script advance time list
+         elseif id == Events.processtick then
+            -- Pass to script process tick list
+         else
+            print("Event: {"..id..","..tostring(context)..","..arg.."}")
+         end
+      end
+      
+      id,context,arg = Events.dequeue(SCTX)
+   end
+
+   -- If we hit end-of-frame, acknowledge it
+   if(id == Events.eof) then
+      Events.framedone(SCTX, context)
+   end
+end
