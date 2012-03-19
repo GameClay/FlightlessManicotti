d1d74bf11a843e10ddc73e372ba96b25de8af6e4
Pat Wilson <zerostride@gmail.com>
2012-03-19 13:19:33 -0700

    Hooray triangle.

--- Cocoa/FlightlessManicotti/FlightlessManicotti/KLAppDelegate.m
+++ Cocoa/FlightlessManicotti/FlightlessManicotti/KLAppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "KLAppDelegate.h"
 #include <FlightlessManicotti/fm.h>
+#include <FlightlessManicotti/render/render.h>
 
 @implementation KLAppDelegate
 
@@ -18,6 +19,7 @@
     NSArray *args = [[NSProcessInfo processInfo] arguments]; // TODO
     if(kl_initialize(KL_FALSE, "main.lua", 0, NULL) == KL_SUCCESS)
     {
+        kl_init_rendering(NULL);
         [[NSRunLoop currentRunLoop] addTimer:
          [NSTimer timerWithTimeInterval:0.0
                                  target:self
@@ -30,6 +32,7 @@
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification
 {
+    kl_destroy_rendering();
     kl_destroy();
 }
 
--- Cocoa/FlightlessManicotti/FlightlessManicotti/KLOpenGLView.m
+++ Cocoa/FlightlessManicotti/FlightlessManicotti/KLOpenGLView.m
@@ -8,27 +8,13 @@
 
 #import "KLOpenGLView.h"
 #include <OpenGL/gl.h>
-
-static void drawAnObject()
-{
-    glColor3f(1.0f, 0.85f, 0.35f);
-    glBegin(GL_TRIANGLES);
-    {
-        glVertex3f(  0.0,  0.6, 0.0);
-        glVertex3f( -0.2, -0.3, 0.0);
-        glVertex3f(  0.2, -0.3 ,0.0);
-    }
-    glEnd();
-}
+#include <FlightlessManicotti/render/render.h>
 
 @implementation KLOpenGLView
 
 -(void) drawRect: (NSRect) bounds
 {
-    glClearColor(0, 0, 0, 0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    drawAnObject();
-    glFlush();
+    kl_render_frame();
 }
 
 @end
