a67389ec49ea546c12b8027a8acb08f723f04fc2
Pat Wilson <zerostride@gmail.com>
2012-04-04 13:34:56 -0700

    I can haz L-systems

--- Cocoa/FlightlessManicotti/main.lua
+++ Cocoa/FlightlessManicotti/main.lua
@@ -13,6 +13,7 @@ end
 require 'events'
 require 'game.components'
 require 'game.components.scene.Scene3DComponent'
+require 'game.Lsystem'
 
 -- Main is executed only once, it is not a loop. It receives the arguments
 -- that were passed to the 'kl_initialize' function.
@@ -30,6 +31,77 @@ function main(...)
    local testscene = Scene3D.new(1024)
 
    testscene = nil
+
+   --lsys_verts = sierpinski(10)
+   lsys_verts = dragoncurve(10)
+end
+
+function dragoncurve(iterations)
+   local lsys = Lsystem.new()
+   lsys:setstart({"F", "X"})
+   lsys:addrule("X", {"X", "+", "Y", "F"})
+   lsys:addrule("Y", {"F", "X", "-", "Y"})
+   local lsys_state = lsys:getstate(iterations)
+
+   local lsys_verts = {}
+   local cx = -0.5
+   local cy = -0.25
+   local ctheta = 1.57079633
+   local fwd = 0.035
+   local dtheta = 1.57079633
+
+   table.insert(lsys_verts, cx)
+   table.insert(lsys_verts, cy)
+
+   for i = 1, #lsys_state do
+      if lsys_state[i] == "F" then
+         cx = cx + fwd * math.cos(ctheta)
+         cy = cy + fwd * math.sin(ctheta)
+
+         table.insert(lsys_verts, cx)
+         table.insert(lsys_verts, cy)
+      elseif lsys_state[i] == "+" then
+         ctheta = ctheta - dtheta
+      elseif lsys_state[i] == "-" then
+         ctheta = ctheta + dtheta
+      end
+   end
+
+   return lsys_verts
+end
+
+function sierpinski(iterations)
+   local lsys = Lsystem.new()
+   lsys:setstart({"A"})
+   lsys:addrule("A", {"B", "-", "A", "-", "B"})
+   lsys:addrule("B", {"A", "+", "B", "+", "A"})
+   local lsys_state = lsys:getstate(iterations)
+
+   local lsys_verts = {}
+   local cx = -0.95
+   local cy = -0.95
+   local ctheta = 0
+   local fwd = 0.0018
+   local dtheta = 1.04719755
+
+   table.insert(lsys_verts, cx)
+   table.insert(lsys_verts, cy)
+
+   for i = 1, #lsys_state do
+      if lsys_state[i] == "A" or lsys_state[i] == "B" then
+         cx = cx + fwd * math.cos(ctheta)
+         cy = cy + fwd * math.sin(ctheta)
+
+         table.insert(lsys_verts, cx)
+         table.insert(lsys_verts, cy)
+      elseif lsys_state[i] == "+" then
+         ctheta = ctheta + dtheta
+      elseif lsys_state[i] == "-" then
+         ctheta = ctheta - dtheta
+      end
+   end
+
+   return lsys_verts
 end
 
 function testinit()
---
+++ script/game/Lsystem.lua
@@ -0,0 +1,97 @@
+-- -*- Mode: lua; tab-width: 3; c-basic-offset: 3; indent-tabs-mode: nil -*-
+-- vim: set filetype=lua tabstop=3 softtabstop=3 shiftwidth=3 expandtab:
+
+-- FlightlessManicotti -- Copyright (C) 2010-2011 GameClay LLC
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+--! @class Lsystem
+Lsystem = {}
+
+--! Constructor.
+--! @memberof Lsystem
+function Lsystem.new()
+   local o = {}
+   setmetatable(o, {__index = Lsystem})
+
+   o._start = {}
+   o._rules = {}
+
+   return o
+end
+
+function Lsystem:setstart(start)
+   self._start = start
+end
+
+function Lsystem:addrule(key, action, weight)
+   local rule = {}
+   rule.action = action
+   rule.weight = weight or 1
+   self._rules[key] = self._rules[key] or {}
+   table.insert(self._rules[key], rule)
+end
+
+function Lsystem.state_to_string(state)
+   local ret = ""
+   if state then
+      for i = 1, #state do
+         ret = ret..state[i]
+      end
+   end
+   return ret
+end
+
+function Lsystem:getstate(iterations, randomseed)
+   local state  = self._start
+
+   --print(Lsystem.state_to_string(state))
+   math.randomseed(randomseed or 42)
+   for n = 1, iterations do
+      local newstate = {}
+      for i = 1, #state do
+         local elem = state[i]
+
+         local newelem = {elem}
+
+         -- Select the correct next element, or keep old element
+         if self._rules[elem] then
+            -- Sum weights of all rules with the same key
+            local sum_weight = 0
+            for j = 1, #self._rules[elem] do
+               sum_weight = self._rules[elem][j].weight + sum_weight
+            end
+
+            -- Pick a random rule
+            local rnd = math.random(sum_weight)
+            sum_weight = 1
+            for j = 1, #self._rules[elem] do
+               if rnd >= sum_weight and rnd < sum_weight + self._rules[elem][j].weight then
+                  newelem = self._rules[elem][j].action
+                  break
+               end
+               sum_weight = self._rules[elem][j].weight + sum_weight
+            end
+         end
+
+         -- Append into newstate
+         for j = 1, #newelem do
+            table.insert(newstate, newelem[j])
+         end
+      end
+      state = newstate
+      --print(Lsystem.state_to_string(state))
+   end
+
+   return state
+end
