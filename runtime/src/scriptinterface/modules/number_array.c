029b28a515d2d93b185bd96dabadd3d478ee3083
Pat Wilson <zerostride@gmail.com>
2012-05-04 15:03:00 -0800

    Changed 'float array' to 'number array' since Lua doesn't care.
    Conflicts:
    
    	runtime/src/beat/freq.c
    	runtime/src/scriptinterface/script.c

--- runtime/src/scriptinterface/modules/number_array.c
+++ runtime/src/scriptinterface/modules/number_array.c
@@ -22,10 +22,10 @@
 #include <lualib.h>
 #include <FlightlessManicotti/fm.h>
 
-#define FLOAT_ARRAY_INSTANCE_TABLE "float_array_instance_method_table"
-const char* FLOAT_ARRAY_LUA_LIB = "float_array";
+#define NUMBER_ARRAY_INSTANCE_TABLE "number_array_instance_method_table"
+const char* NUMBER_ARRAY_LUA_LIB = "number_array";
 
-static int float_array_index(lua_State* L)
+static int number_array_index(lua_State* L)
 {
    size_t len;
    const char* key;
@@ -41,13 +41,13 @@ static int float_array_index(lua_State* L)
    }
 
    /* Wasn't x or y, so check the instance table */
-   lua_getglobal(L, FLOAT_ARRAY_INSTANCE_TABLE);
+   lua_getglobal(L, NUMBER_ARRAY_INSTANCE_TABLE);
    lua_pushvalue(L, 2);
    lua_gettable(L, -2);
    return lua_isnil(L, -1) ? 0 : 1;
 }
 
-static int float_array_newindex(lua_State* L)
+static int number_array_newindex(lua_State* L)
 {
    size_t len;
    int idx;
@@ -70,25 +70,24 @@ static int float_array_newindex(lua_State* L)
    return 0;
 }
 
-static const struct luaL_reg float_array_instance_methods [] = {
+static const struct luaL_reg number_array_instance_methods [] = {
    {NULL, NULL}
 };
 
-static const struct luaL_reg float_array_class_methods [] = {
+static const struct luaL_reg number_array_class_methods [] = {
    {NULL, NULL}
 };
 
-int luaopen_float_array(lua_State* L)
+int luaopen_number_array(lua_State* L)
 {
-   luaL_register(L, FLOAT_ARRAY_INSTANCE_TABLE, float_array_instance_methods);
-
-   luaL_newmetatable(L, FLOAT_ARRAY_LUA_LIB);
-   lua_pushcfunction(L, float_array_index);
+   luaL_newmetatable(L, NUMBER_ARRAY_LUA_LIB);
+   lua_pushcfunction(L, number_array_index);
    lua_setfield(L, -2, "__index");
-   lua_pushcfunction(L, float_array_newindex);
+   lua_pushcfunction(L, number_array_newindex);
    lua_setfield(L, -2, "__newindex");
+   luaL_register(L, NUMBER_ARRAY_INSTANCE_TABLE, number_array_instance_methods);
 
-   luaL_register(L, FLOAT_ARRAY_LUA_LIB, float_array_class_methods);
+   luaL_register(L, NUMBER_ARRAY_LUA_LIB, number_array_class_methods);
 
    return 1;
 }
