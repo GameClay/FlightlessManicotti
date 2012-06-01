34b00375d5ca7de9e3b38af890957738d0a60cbb
Pat Wilson <zerostride@gmail.com>
2012-06-01 14:38:29 -0800

    Reformat to tab size 3, add guard for shutting down to prevent crash on exit.
    Conflicts:
    
    	Cocoa/FlightlessManicotti/FlightlessManicotti/KLOpenGLView.m

--- Cocoa/FlightlessManicotti/FlightlessManicotti/KLAppDelegate.m
+++ Cocoa/FlightlessManicotti/FlightlessManicotti/KLAppDelegate.m
@@ -28,108 +28,112 @@ ALint sample;
 ALCdevice *device = NULL;
 BOOL g_loop_last_buffer = FALSE;
 
+BOOL g_shutting_down = FALSE;
+
 @implementation KLAppDelegate
 
 @synthesize window = _window;
 
 - (id)init 
 {
-    self = [super init];
-    if (self)
-    {
-        NSArray *args = [[NSProcessInfo processInfo] arguments]; // TODO
-        if(kl_initialize(KL_FALSE, // Threaded script processing
-                         KL_FALSE, // Don't wait on script fences
-                         "main.lua", // main script
-                         0, // No args yet
-                         NULL) == KL_SUCCESS)
-        {
-            [[NSRunLoop currentRunLoop] addTimer:
-             [NSTimer timerWithTimeInterval:0.0
-                                     target:self
-                                   selector:@selector(update:)
-                                   userInfo:nil
-                                    repeats:YES]
-                                    forMode:NSRunLoopCommonModes];
-        }
-        
-        //kl_matrix_math_self_test(); // Hax
-        //kl_vector_math_self_test();
-        
-        // Massive hax
-        alGetError();
-        device = alcCaptureOpenDevice(NULL, SRATE, AL_FORMAT_STEREO16, SSIZE);
-        alcCaptureStart(device);
-    }
-    return self;
+   self = [super init];
+   if (self)
+   {
+      NSArray *args = [[NSProcessInfo processInfo] arguments]; // TODO
+      if(kl_initialize(KL_FALSE, // Threaded script processing
+                       KL_FALSE, // Don't wait on script fences
+                       "main.lua", // main script
+                       0, // No args yet
+                       NULL) == KL_SUCCESS)
+      {
+         [[NSRunLoop currentRunLoop] addTimer:
+          [NSTimer timerWithTimeInterval:0.0
+                                  target:self
+                                selector:@selector(update:)
+                                userInfo:nil
+                                 repeats:YES]
+                                      forMode:NSRunLoopCommonModes];
+      }
+      
+      //kl_matrix_math_self_test(); // Hax
+      //kl_vector_math_self_test();
+      
+      // Massive hax
+      alGetError();
+      device = alcCaptureOpenDevice(NULL, SRATE, AL_FORMAT_STEREO16, SSIZE);
+      alcCaptureStart(device);
+   }
+   return self;
 }
 
 - (void)dealloc
 {
-    [_window release];
-    [super dealloc];
+   [_window release];
+   [super dealloc];
 }
 
 - (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender
 {
-    // Massive hax
-    alcCaptureStop(device);
-    alcCaptureCloseDevice(device);
-    device = NULL;
-
-    kl_predestroy();
-    [[NSRunLoop currentRunLoop] addTimer:
-     [NSTimer timerWithTimeInterval:0.5
-                             target:self
-                           selector:@selector(doTerminate:)
-                           userInfo:nil
-                            repeats:NO]
-                            forMode:NSModalPanelRunLoopMode];
-
-    return NSTerminateLater;
+   // Massive hax
+   alcCaptureStop(device);
+   alcCaptureCloseDevice(device);
+   device = NULL;
+   
+   g_shutting_down = TRUE;
+   
+   kl_predestroy();
+   [[NSRunLoop currentRunLoop] addTimer:
+    [NSTimer timerWithTimeInterval:0.5
+                            target:self
+                          selector:@selector(doTerminate:)
+                          userInfo:nil
+                           repeats:NO]
+                                forMode:NSModalPanelRunLoopMode];
+   
+   return NSTerminateLater;
 }
 
 extern uint64_t hax_sum_frame_time_ns;
 extern uint64_t hax_num_frames;
 - (void)applicationWillTerminate:(NSNotification *)aNotification
 {
-    /* slightly moar hax */
-    NSLog(@"Average frame time: %f", ((double)hax_sum_frame_time_ns / hax_num_frames) * 1e-6);
+   /* slightly moar hax */
+   NSLog(@"Average frame time: %f", ((double)hax_sum_frame_time_ns / hax_num_frames) * 1e-6);
 
-    kl_destroy();
+   kl_destroy();
 }
 
 -(void)doTerminate:(NSTimer *)theTimer
 {
-    [NSApp replyToApplicationShouldTerminate:YES];
+   [NSApp replyToApplicationShouldTerminate:YES];
 }
 
 -(void)update:(NSTimer *)theTimer
 {
-    // Massive hax
-    if(device != NULL)
-    {
-        if(g_loop_last_buffer)
-        {
-            kl_freq_manager_update(KL_DEFAULT_FREQ_MANAGER, buffer, SSIZE);
-        }
-        else
-        {
-            alcGetIntegerv(device, ALC_CAPTURE_SAMPLES, (ALCsizei)sizeof(ALint), &sample);
-            if(sample == SSIZE)
-            {
-                alcCaptureSamples(device, (ALCvoid *)buffer, sample);
-                kl_freq_manager_update(KL_DEFAULT_FREQ_MANAGER, buffer, sample);
-            }
-        }
-    }
+   // Massive hax
+   if(device != NULL)
+   {
+      if(g_loop_last_buffer)
+      {
+         kl_freq_manager_update(KL_DEFAULT_FREQ_MANAGER, buffer, SSIZE);
+      }
+      else
+      {
+         alcGetIntegerv(device, ALC_CAPTURE_SAMPLES, (ALCsizei)sizeof(ALint), &sample);
+         if(sample == SSIZE)
+         {
+            alcCaptureSamples(device, (ALCvoid *)buffer, sample);
+            kl_freq_manager_update(KL_DEFAULT_FREQ_MANAGER, buffer, sample);
+         }
+      }
+   }
 }
 
 -(IBAction)onTextEntry:(id)sender
 {
-    NSString* text = [sender stringValue];
-    kl_script_console_input(KL_DEFAULT_SCRIPT_CONTEXT, [text UTF8String]);
-    [sender setStringValue:@""];
+   NSString* text = [sender stringValue];
+   kl_script_console_input(KL_DEFAULT_SCRIPT_CONTEXT, [text UTF8String]);
+   [sender setStringValue:@""];
 }
 
 @end
--- Cocoa/FlightlessManicotti/FlightlessManicotti/KLOpenGLView.m
+++ Cocoa/FlightlessManicotti/FlightlessManicotti/KLOpenGLView.m
@@ -9,92 +9,146 @@
 #import "KLOpenGLView.h"
 #include <OpenGL/gl.h>
 
+/* slightly moar hax */
+#define KL_OS_DARWIN
+#include <FlightlessManicotti/core/timer.h>
+uint64_t hax_sum_frame_time_ns = 0;
+uint64_t hax_num_frames = 0;
+
+extern BOOL g_loop_last_buffer;
+
+extern BOOL g_shutting_down;
+
 @implementation KLOpenGLView
 
 // This is the renderer output callback function
-static CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTimeStamp* now, const CVTimeStamp* outputTime,
-                                      CVOptionFlags flagsIn, CVOptionFlags* flagsOut, void* displayLinkContext)
+static CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTimeStamp* now,
+                                      const CVTimeStamp* outputTime, CVOptionFlags flagsIn,
+                                      CVOptionFlags* flagsOut, void* displayLinkContext)
 {
-    CVReturn result = [(KLOpenGLView*)displayLinkContext getFrameForTime:outputTime];
-    return result;
+   CVReturn result = [(KLOpenGLView*)displayLinkContext getFrameForTime:outputTime];
+   return result;
 }
 
 - (id)initWithFrame:(NSRect)frame
 {
-    NSOpenGLPixelFormatAttribute attrs[] =
-    {
-        //NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion3_2Core,
-        NSOpenGLPFADoubleBuffer,
-        NSOpenGLPFADepthSize, 32,
-        0
-    };
-
-    NSOpenGLPixelFormat* pixFmt = [[NSOpenGLPixelFormat alloc] initWithAttributes:attrs];
-
-    self = [super initWithFrame:frame pixelFormat:pixFmt];
-
-    if(!self) return nil;
-
-    return self;
+   NSOpenGLPixelFormatAttribute attrs[] =
+   {
+      //NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion3_2Core,
+      NSOpenGLPFADoubleBuffer,
+      NSOpenGLPFADepthSize, 32,
+      0
+   };
+   
+   NSOpenGLPixelFormat* pixFmt = [[NSOpenGLPixelFormat alloc] initWithAttributes:attrs];
+   
+   self = [super initWithFrame:frame pixelFormat:pixFmt];
+   
+   if(!self) return nil;
+   
+   return self;
 }
 
 - (void)reshape
 {
-    NSRect bounds = [self bounds];
-    displayWidth = NSWidth(bounds);
-    displayHeight = NSHeight(bounds);
+   NSRect bounds = [self bounds];
+   displayWidth = NSWidth(bounds); 
+   displayHeight = NSHeight(bounds);
+   kl_render_reshape(renderContext, displayWidth, displayHeight);
 }
 
 - (void)prepareOpenGL
 {
 #if 0
-    // Synchronize buffer swaps with vertical refresh rate
-    GLint swapInt = 1;
-    [[self openGLContext] setValues:&swapInt forParameter:NSOpenGLCPSwapInterval];
+   // Synchronize buffer swaps with vertical refresh rate
+   GLint swapInt = 1;
+   [[self openGLContext] setValues:&swapInt forParameter:NSOpenGLCPSwapInterval];
 #endif
-
-    // Create a display link capable of being used with all active displays
-    CVDisplayLinkCreateWithActiveCGDisplays(&displayLink);
-
-    // Set the renderer output callback function
-    CVDisplayLinkSetOutputCallback(displayLink, &MyDisplayLinkCallback, self);
-
-    // Set the display link for the current renderer
-    CGLContextObj cglContext = [[self openGLContext] CGLContextObj];
-    CGLPixelFormatObj cglPixelFormat = [[self pixelFormat] CGLPixelFormatObj];
-    CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(displayLink, cglContext, cglPixelFormat);
-
-    // Init the rendering
-    kl_init_rendering(&renderContext, (void*)cglContext);
-
-    NSString *versionString = [NSString stringWithCString:(const char*)glGetString(GL_VERSION)encoding:NSASCIIStringEncoding];
-    NSLog(@"OpenGL Version: %@", versionString);
-
-    // Activate the display link
-    CVDisplayLinkStart(displayLink);
+   
+   // Create a display link capable of being used with all active displays
+   CVDisplayLinkCreateWithActiveCGDisplays(&displayLink);
+   
+   // Set the renderer output callback function
+   CVDisplayLinkSetOutputCallback(displayLink, &MyDisplayLinkCallback, self);
+   
+   // Set the display link for the current renderer
+   CGLContextObj cglContext = [[self openGLContext] CGLContextObj];
+   CGLPixelFormatObj cglPixelFormat = [[self pixelFormat] CGLPixelFormatObj];
+   CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(displayLink, cglContext, cglPixelFormat);
+   
+   // Init the rendering
+   kl_init_rendering(&renderContext, (void*)cglContext);
+   
+   NSString *versionString = [NSString stringWithCString:(const char*)glGetString(GL_VERSION)encoding:NSASCIIStringEncoding];
+   NSLog(@"OpenGL Version: %@", versionString);
+   
+   // Activate the display link
+   CVDisplayLinkStart(displayLink);
 }
 
 - (CVReturn)getFrameForTime:(const CVTimeStamp*)outputTime
 {
-    // Mainloop iteration
-    kl_mainloop_iteration();
-
-    // draw here
-    if(renderContext != NULL)
-        kl_render_frame(renderContext, displayWidth, displayHeight);
-    
-    return kCVReturnSuccess;
+   // Mainloop iteration
+   if(!g_shutting_down)
+   {
+      kl_mainloop_iteration();
+      
+      // draw here
+      if(renderContext != NULL)
+      {
+         /* hax */
+         kl_absolute_time_t frame_timestamp_start, frame_timestamp_end;
+         uint64_t frame_time_ns;
+         kl_high_resolution_timer_query(&frame_timestamp_start);
+         
+         kl_render_frame(renderContext, displayWidth, displayHeight);
+         
+         /* hax */
+         {
+            kl_high_resolution_timer_query(&frame_timestamp_end);
+            frame_timestamp_end -= frame_timestamp_start;
+            kl_absolute_time_to_ns(&frame_timestamp_end, &frame_time_ns);
+            hax_sum_frame_time_ns += frame_time_ns;
+            hax_num_frames++;
+         }
+      }
+   }
+   
+   return kCVReturnSuccess;
 }
 
 - (void)dealloc
 {
-    // Release the display link
-    CVDisplayLinkRelease(displayLink);
-    
-    // Destroy KL rendering
-    kl_destroy_rendering(&renderContext);
-    
-    [super dealloc];
+   // Release the display link
+   CVDisplayLinkRelease(displayLink);
+   
+   // Destroy KL rendering
+   kl_destroy_rendering(&renderContext);
+   
+   [super dealloc];
+}
+
+- (BOOL)acceptsFirstResponder
+{
+   return YES;
+}
+
+-(void)keyDown:(NSEvent*)event
+{
+   switch([event keyCode])
+   {
+      case 1: // 's'
+         kl_render_spectrum_setting++;
+         if(kl_render_spectrum_setting > 5) kl_render_spectrum_setting = 0;
+         break;
+         
+      case 37: // 'l'
+         g_loop_last_buffer = !g_loop_last_buffer;
+         break;
+      default:
+         NSLog(@"Key %@", event);
+         break;
+   }
 }
 
 @end
